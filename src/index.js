0c5a033c0391799813884567e9d073431e550947
AlenaBDE <bde.alena@gmail.com>
2018-03-06 19:11:03 +0300

    8

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
-}
+
+
+    var count = 1;
+    var maxCount = 0;
+
+    array.sort(function (a, b) {
+        return a - b
+    });
+
+    if (array.length == 1) {
+        return 1;
+    }
+
+    for (var i = 0; i < array.length - 1; i++) {
+        if ((array[i + 1] - array[i]) < 2) {
+            count++
+        }
+        else
+            count = 1;
+
+        if (array[i + 1] == array[i]) {
+            count--
+        }
+
+        if (maxCount < count) {
+            maxCount = count;
+        }
+
+    }
+    return maxCount;
+};
